9ff31e728445e94bc9d95a74667bea8fb2fa7930
christ_2014 <christ_2014@foxmail.com>
2022-02-23 14:21:55 +0000

    update WebSocketServer/src/WebSocketServer.cpp.

--- WebSocketServer/src/WebSocketServer.cpp
+++ WebSocketServer/src/WebSocketServer.cpp
@@ -9,3 +9,43 @@ WebSocketServer::WebSocketServer(const Server<websocketpp::connection_hdl, Messa
                                  const Server<websocketpp::connection_hdl, Message, User>::CheckAuth &checkAuth,
                                  const Server<websocketpp::connection_hdl, Message, User>::DisposeClient &disposeClient)
         : Server(decoder, encoder, checkAuth, disposeClient) {}
+
+WebSocketServer::WebSocketServer() {
+
+}
+
+void WebSocketServer::init() {
+    _decoder = [&](std::string text){
+        return decoder(text);
+    };
+    _encoder = [&](Message msg){
+        return _encoder(msg);
+    };
+    _checkAuth = [&](std::string text){
+        return checkAuth(text);
+    };
+    _disposeClient = [&](auto client){
+        disposeClient(client);
+    };
+}
+
+Message WebSocketServer::decoder(std::string text) {
+    auto dynamic = folly::parseJson(text);
+    return {dynamic["type"].asString(),dynamic["content"].asString()};
+}
+
+std::string WebSocketServer::encoder(Message msg) {
+    folly::dynamic dymiac;
+    dymiac["type"]=msg.type;
+    dymiac["content"]=msg.content;
+    return folly::toJson(dymiac);
+}
+
+User WebSocketServer::checkAuth(std::string text) {
+    auto msg = decoder(text);
+    return {msg.type,msg.content};
+}
+
+void WebSocketServer::disposeClient(websocketpp::connection_hdl client) {
+
+}
